f3b713238e5301ccdeac240372aba53d1bf15ccb
GustavoMendozaAren <stavmdza@gmail.com>
2025-07-02 09:03:31 -0600

    Build2

--- MachinaUnityProject/Assets/6.Scripts/1.MainCharScripts/MCPush.cs
+++ MachinaUnityProject/Assets/6.Scripts/1.MainCharScripts/MCPush.cs
@@ -1,29 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class MCPush : MonoBehaviour
 {
+    [SerializeField] GameObject[] pressEImg;
+    [SerializeField] Collider eCollider;
+    private bool isEPressed = false;
+
     [SerializeField] float interactRange = 2f;
-    public KeyCode interactKey = KeyCode.E;
 
     private PushableObject currentPushable;
 
     Ray ray;
-    PushableObject pushable;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //other = eCollider;
+        if (other.CompareTag("MovObj"))
+        {
+            if (!isEPressed)
+                pressEImg[0].SetActive(true);
+            else if (isEPressed) 
+                pressEImg[0].SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //other = eCollider;
+        if (other.CompareTag("MovObj"))
+        {
+            pressEImg[0].SetActive(false);
+        }
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(interactKey))
+        //Debug.DrawRay(transform.position, transform.forward * 2, Color.white);
+        if (Input.GetKeyDown(KeyCode.E))
         {
             if (currentPushable == null)
             {
                 TryStartPush();
+                isEPressed = true;
+                pressEImg[0].SetActive(false);
+                pressEImg[1].SetActive(true);
             }
             else
             {
+                isEPressed = false;
                 currentPushable.StopPush();
                 currentPushable = null;
+                pressEImg[1].SetActive(false);
             }
         }
     }
@@ -31,10 +61,10 @@ public class MCPush : MonoBehaviour
     void TryStartPush()
     {
         ray = new Ray(transform.position + Vector3.up, transform.forward);
-        Debug.DrawRay(transform.position, transform.forward * 2, Color.white);
+        
         if (Physics.Raycast(ray, out RaycastHit hit, interactRange))
         {
-            pushable = hit.collider.GetComponent<PushableObject>();
+            PushableObject pushable = hit.collider.GetComponent<PushableObject>();
             if (pushable != null)
             {
                 currentPushable = pushable;
